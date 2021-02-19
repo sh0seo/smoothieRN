1e5d6cc5a969e5f47af519253ec6ebfaa776d969
sh0seo <ssh0702@gmail.com>
2021-02-19 16:00:01 +0900

    initalize chapter3

---
+++ FirstApp/src/App.tsx
@@ -0,0 +1,139 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StyleSheet, View, Text, StatusBar} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import Styled from 'styled-components';
+
+declare const global: {HermesInternal: null | {}};
+
+const ScrollView = Styled.ScrollView`
+  background-color: ${Colors.lighter};
+`;
+
+const Body = Styled.View`
+  background-color: ${Colors.white};
+`;
+
+const SectionContainer = Styled.View`
+  margin-top: 32px;
+  padding-horizontal: 24px;
+`;
+
+const SectionDescription = Styled.Text`
+  margin-top: 8px;
+  font-size: 18px;
+  font-weight: 400;
+  color: ${Colors.dark};
+`;
+
+const HighLight = Styled.Text`
+  font-weight: 700;
+`;
+
+interface Props {}
+
+const App = ({}: Props) => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <Body>
+            <SectionContainer>
+              <Text style={styles.sectionTitle}>Step One</Text>
+              <SectionDescription>
+                Edit <HighLight>App.tsx</HighLight> to change this
+                screen and then come back to see your edits.
+              </SectionDescription>
+            </SectionContainer>
+            <SectionContainer>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <SectionDescription>
+                <ReloadInstructions />
+              </SectionDescription>
+            </SectionContainer>
+            <SectionContainer>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <SectionDescription>
+                <DebugInstructions />
+              </SectionDescription>
+            </SectionContainer>
+            <SectionContainer>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <SectionDescription>
+                Read the docs to discover what to do next:
+              </SectionDescription>
+            </SectionContainer>
+            <LearnMoreLinks />
+          </Body>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
