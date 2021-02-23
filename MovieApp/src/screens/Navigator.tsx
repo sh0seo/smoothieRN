dfc7c415451ffaee80e147644ed554eb4d3b0254
sh0seo <ssh0702@gmail.com>
2021-02-23 20:03:02 +0900

    deving MovieApp

--- MovieApp/src/screens/Navigator.tsx
+++ MovieApp/src/screens/Navigator.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import { UserContext } from '~/contexts/User';
+import Loading from './Loading';
 
 const Navigator = () => {
-  return 0;
+  const {isLoading, userInfo} = useContext<IUserContext>(UserContext);
+
+  if (isLoading === false) {
+    return <Loading />
+  }
+
+  return (
+    <NavigationContainer>
+      {userInfo ? <MovieNavigator /> : <LoginNavigator />}
+    </NavigationContainer>
+  );
 }
 
 export default Navigator;
