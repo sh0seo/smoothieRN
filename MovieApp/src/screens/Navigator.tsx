4febce5b45b59cf4046d8d94004aba0f6594d948
sh0seo <ssh0702@gmail.com>
2021-02-23 19:37:47 +0900

    deving MovieApp

---
+++ MovieApp/src/screens/Navigator.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Navigator = () => {
+  return 0;
+}
+
+export default Navigator;
